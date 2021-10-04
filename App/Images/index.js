cff1001260d020e7ad1f8cd84d2646f70f6beaa0
Anggasayogo <anggasayogosm@gmail.com>
2021-10-04 20:29:53 +0700

    custom assets

--- App/Images/index.js
+++ App/Images/index.js
@@ -1,5 +1,18 @@
 const Images = {
-  appLogo: require('./logo.png')
+  appLogo: require('./logo.png'),
+  icBell: require('./icon/bell.png'),
+  icProfile: require('./icon/profile.png'),
+
+  // Weather
+  icCerahBerawan: require('./weathericon/cerahBerawan.png'),
+  icMatahari: require('./weathericon/sun.png'),
+  icBerawan: require('./weathericon/berawan.png'),
+  icBulan: require('./weathericon/berawan.png'),
+  icHujanMalam: require('./weathericon/weatherMoon.png'),
+  icMalamBerawan: require('./weathericon/moonBerawan.png'),
+  icHujanPetir: require('./weathericon/hujanPetir.png'),
+  icHujan: require('./weathericon/hujan.png'),
+  icCerah: require('./weathericon/cerah.png'),
 }
 
 export default Images
